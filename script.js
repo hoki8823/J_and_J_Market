f33442068557f100eaf8367c1d602a88f8b7ece8
JaeMan Kim <66447438+hoki8823@users.noreply.github.com>
2021-10-02 02:13:29 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var toogleBtn = document.querySelector('.navbar_toogleBtn');
-var menu = document.querySelector('.navbar_menu');
-var icons = document.querySelector('.navbar_icons');
-
-toogleBtn.addEventListener('click',function(){
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
